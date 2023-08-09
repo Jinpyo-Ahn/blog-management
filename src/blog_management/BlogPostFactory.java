8a9a9be77c3cc11ca9bbe097885301807591f651
unknown <dkswlsvy3312@gmail.com>
2023-08-09 11:05:54 +0900

    게시물 객체 생성 팩토리 클래스

--- src/blog_management/BlogPostFactory.java
+++ src/blog_management/BlogPostFactory.java
@@ -2,10 +2,11 @@ package blog_management;
 
 // BlogPostFactory 클래스 -> 게시물 객체를 생성하는 역할
 public class BlogPostFactory {
+
     // creatPost() 메서드
     // 게시물의 종류(postType)와 제목, 내용을 입력받아
     // 해당 종류에 맞는 게시물 객체를 생성
-    // 만약 image 타입의 게시물이면, TagDecorator로 감싸져 객체가 생성
+    // 만약 '사진 게시물' 타입의 게시물이면, TagDecorator로 감싸져 객체가 생성
     public BlogPost createPost(String postType, String title, String content) {
         if ("기본 게시물".equals(postType)) {
             return new BlogPost(title, content);

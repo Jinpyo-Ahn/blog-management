2d42aa60c2277796a1c6e1beab7e7e0f6ed7a7ab
unknown <dkswlsvy3312@gmail.com>
2023-08-15 17:23:44 +0900

    태그 기능 데코레이터 클래스

---
+++ src/blog_management/addition/TagDecorator.java
@@ -0,0 +1,26 @@
+package blog_management.addition;
+
+import blog_management.BlogPost;
+
+// TagDecorator 데코레이터 클래스 -> BlogPost 객체를 감싸 '태그' 기능을 추가
+public class TagDecorator extends BlogPost {
+    final String[] tags;
+
+    // 생성자에서 기존 게시물과 태그 배열을 받아 저장
+    public TagDecorator(BlogPost post, String[] tags) {
+        super(post.getTitle(), post.getContent());
+        this.tags = tags;
+    }
+
+    // display() 메서드에를 오버라이드 해 기존 게시물의 내용을 출력한 뒤, 태그를 출력
+    @Override
+    public void display() {
+        super.display();
+        System.out.print("태그(#): ");
+        for (String tag : tags) {
+            System.out.print(tag + " ");
+        }
+        System.out.println();
+    }
+
+}

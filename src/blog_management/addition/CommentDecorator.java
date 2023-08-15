48d0cabad3172b9f3e29e2f99dcd07f295f16910
unknown <dkswlsvy3312@gmail.com>
2023-08-15 17:22:52 +0900

    댓글 기능 데코레이터 클래스

---
+++ src/blog_management/addition/CommentDecorator.java
@@ -0,0 +1,21 @@
+package blog_management.addition;
+
+import blog_management.BlogPost;
+
+// CommentDecorator 데코레이터 클래스 -> BlogPost 객체를 감싸 '댓글' 기능을 추가
+public class CommentDecorator extends BlogPost {
+    final String comment;
+
+    // 생성자에서 기존 게시물과 댓글을 받아 저장
+    public CommentDecorator(BlogPost post, String comment) {
+        super(post.getTitle(), post.getContent());
+        this.comment = comment;
+    }
+
+    // display() 메서드를 오버라이드 해 기존 게시물의 내용을 출력한 뒤, 댓글을 출력
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("댓글: " + this.comment);
+    }
+}
